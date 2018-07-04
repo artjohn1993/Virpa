ceebeca08a247e02ceffeaee52684e414a4afcc4
artjohn <artjohn1993@gmail.com>
2018-07-04 15:52:20 +0800

    add navigation bar home activity

--- app/src/main/java/com/local/virpa/virpa/activity/HomeActivity.kt
+++ app/src/main/java/com/local/virpa/virpa/activity/HomeActivity.kt
@@ -2,12 +2,42 @@ package com.local.virpa.virpa.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.local.virpa.virpa.R
+import kotlinx.android.synthetic.main.activity_home.*
+import android.support.annotation.NonNull
+import android.support.design.widget.BottomNavigationView
+
+
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        supportActionBar?.elevation = 0F
+        title = ""
+
+        navigationBar.setOnNavigationItemSelectedListener(
+                BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                    when (item.itemId) {
+                        R.id.sample1 -> {
+
+                        }
+                        R.id.sample2 -> {
+
+                        }
+                        R.id.sample3 -> {
+
+                        }
+                        R.id.sample4 -> {
+
+                        }
+                        R.id.sample5 -> {
+
+                        }
+                    }
+                    true
+                })
     }
 }
